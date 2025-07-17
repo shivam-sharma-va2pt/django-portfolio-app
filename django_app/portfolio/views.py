45768bfa05dc420e43fc9c8ccc557e774c42552f
Shivam Sharma <shivam.sharma@va2pt.com>
2025-07-17 18:24:07 +0530

    dghd

--- django_app/portfolio/views.py
+++ django_app/portfolio/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Shivam Portfolio</h1><p>Complete Hoogaya</p>")
+    return HttpResponse("<h1>Shivam Portfolio</h1><p>Complfdhgete Hoogaya</p>")
 # Create your views here.
